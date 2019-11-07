06efe0c939f9b778542490360bae2cc6596d7921
meng.wm <meng.wm@alibaba-inc.com>
2019-11-07 14:04:20 +0800

    update demo

--- src/demo3/index.js
+++ src/demo3/index.js
@@ -1,28 +1,61 @@
 const IMG_SRC_1 = require('../test-images/test1.png');
 const IMG_SRC_2 = require('../test-images/test2.png');
 const IMG_SRC_3 = require('../test-images/test3.jpg');
-import GLImage  from './gl-image';
+import GLImage from 'gl-image';
+
+let resEle = document.createElement('div');
+resEle.style.position = 'fixed';
+resEle.style.top = '0';
+resEle.style.right = '0';
+resEle.style.width = '300px';
+document.body.appendChild(resEle);
 
 
-const style = document.createElement('style');
-style.innerHTML = 'html body { padding: 0; margin: 0;}';
-document.head.appendChild(style);
 
 
-const canvasEle = document.createElement('canvas');
-document.body.appendChild(canvasEle);
+let glImage = new GLImage();
 
+// glImage.loadImageSrc(IMG_SRC_1).then(() => {
+  
+// })
 
-let glImage = new GLImage(canvasEle);
-let canvas = glImage.getCanvas();
-document.body.appendChild(canvas);
+const imageSrcList = [IMG_SRC_1, IMG_SRC_2, IMG_SRC_3];
 
-window.glImage = glImage;
+async function processSingle(imageSrc) {
+  await glImage.loadImageSrc(imageSrc);
+  glImage.applyFilters({
+    'brightness': -0.2,
+    'saturation': -0.7
+  });
+  return glImage.toDataUrl();
+}
 
+async function batchProcess(imageSrcList) {
+  const result = [];
+  for (let i = 0; i < imageSrcList.length; i++) {
+    const res = await processSingle(imageSrcList[i]);
+    result.push(res);
+  }
+  return result;
+}
 
-glImage.loadImageSrc(IMG_SRC_1).then(() => {
-  glImage.draw();
+batchProcess(imageSrcList).then((result) => {
+  console.log(result);
+  result.forEach((base64) => {
+    const img = new Image();
+    img.src = base64;
+    img.style.width = '100%';
+
+    resEle.appendChild(img);
+  })
 })
 
 
 
+let canvas = glImage.getCanvas();
+document.body.appendChild(canvas);
+
+
+window.glImage = glImage;
+window.SRC_2 = IMG_SRC_2;
+window.SRC_3 = IMG_SRC_3;
